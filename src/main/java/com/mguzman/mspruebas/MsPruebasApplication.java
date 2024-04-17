2c828bc90696a1d5e52563e91f13058fc9909c9b
mguzman412 <mguzman412@gmail.com>
2024-04-17 00:15:41 -0400

    feat: first commit

---
+++ src/main/java/com/mguzman/mspruebas/MsPruebasApplication.java
@@ -0,0 +1,13 @@
+package com.mguzman.mspruebas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPruebasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPruebasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mguzman/mspruebas/MsPruebasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mguzman.mspruebas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPruebasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
